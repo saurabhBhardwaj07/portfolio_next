009bb5e7b030434d5faca2d17560696d79b9f9f0
saurabhFCTS <saurabh.kumar@forwardcode.in>
2024-10-11 17:00:49 +0530

    Create layout.tsx
    Co-Authored-By: saurabhFCTS <114730019+saurabhFCTS@users.noreply.github.com>

---
+++ app/projects/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+import AppTheme from "@/theme/AppTheme";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 700",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 700",
+});
+
+export const metadata: Metadata = {
+  title: "Create Next App",
+  description: "Working on portfollio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <AppTheme>
+        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          {children}
+        </body>
+      </AppTheme>
+    </html>
+  );
+}
